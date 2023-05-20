0186da1eccddd2afa11ccaf1ac68a128c3cf59aa
mellysujakto <msujakto@gmail.com>
2023-05-20 11:12:25 +0700

    Initial commit

---
+++ app/src/main/java/com/undira/tiundira/MainActivity.java
@@ -0,0 +1,32 @@
+package com.undira.tiundira;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import com.undira.tiundira.ui.main.SectionsPagerAdapter;
+import com.undira.tiundira.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     binding = ActivityMainBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+    }
+}
